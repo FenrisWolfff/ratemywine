b59fa4f566694fddd9e18e27515436a8f8952ece
Fenris Lu <fenrisludwig@gmail.com>
2024-05-30 13:40:35 -0700

    added TODOs

--- app/views.py
+++ app/views.py
@@ -28,6 +28,7 @@ def library(request):
 
 def wine_detail(request, pk):
     if request.user.is_authenticated:
+        # TODO: add all reviews related to this wine to the page with the provided template
         return render(request, "wine_detail.html", {'wine': Wine.objects.get(pk=pk)})
     else:
         return redirect('login')
@@ -36,6 +37,8 @@ def wine_detail(request, pk):
 def forum(request):
     if request.user.is_authenticated:
         if request.POST:
+            # TODO: sorting/filtering reviews customized by user
+
             pass
         else:
             return render(request, "forum.html", {})
@@ -45,13 +48,26 @@ def forum(request):
 
 def my_reviews(request):
     if request.user.is_authenticated:
-        return render(request, "my_reviews.html", {})
+        if request.POST:
+            # TODO: sorting my reviews customized by user
+
+            pass
+        else:
+            return render(request, "my_reviews.html", {})
     else:
         return redirect('login')
 
 
 def post_review(request):
     if request.user.is_authenticated:
-        return render(request, "post_review.html", {})
+        if request.POST:
+            # TODO: update review to database and redirect to my_reviews page
+            # TODO: If review exists for the wine-user combo passed in, populate post_review.html
+            #  with existing review and update existing review in the database. If review doesn't
+            #  exist, create blank page and create new review in the database.
+
+            pass
+        else:
+            return render(request, "post_review.html", {})
     else:
         return redirect('login')
